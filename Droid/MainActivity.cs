5a0b2bcd29dc1c802c71ddcd9428efa2dfdae3a3
Dave Fishel <dave@anki.com>
2018-02-15 09:23:55 -0800

    Add native android crash to the mix

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -6,11 +6,19 @@ using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
 using System;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace Wicket.Droid {
+
   [Activity(Label = "Wicket", MainLauncher = true, Icon = "@mipmap/icon")]
   public class MainActivity : Activity {
     int count = 1;
+
+    [DllImport("libnative-lib.so")]
+    public extern static int TestMethod();
+
+    [DllImport("libnative-lib.so")]
+    public extern static void TestCrash();
 
     protected override void OnCreate(Bundle savedInstanceState) {
       base.OnCreate(savedInstanceState);
@@ -31,24 +39,59 @@ namespace Wicket.Droid {
         Console.WriteLine("crashes not enabled!");
       }
 
-      Button crashButton = FindViewById<Button>(Resource.Id.crashButton);
-      crashButton.Click += delegate {
-        Console.WriteLine("Attempting to crash our own app!");
-        CrashMe3();
+      Crashes.ShouldProcessErrorReport = (ErrorReport report) => {
+        // Check the report in here and return true or false depending on the ErrorReport.
+        Console.WriteLine("Here1");
+        return true;
       };
 
+      var task2 = Task.Run(async () => await Crashes.HasCrashedInLastSessionAsync());
+      bool crashed = task.Result;
+      if (crashed) {
+        Console.WriteLine("crashed last session!");
+      }
+
+      Crashes.SendingErrorReport += (sender, e) => {
+        // Your code, e.g. to present a custom UI.
+        Console.WriteLine("Here2");
+      };
+
+      Crashes.SentErrorReport += (sender, e) => {
+        // Your code, e.g. to hide the custom UI.
+        Console.WriteLine("Here3");
+      };
+
+      Crashes.FailedToSendErrorReport += (sender, e) => {
+        // Your code goes here.
+        Console.WriteLine("Here4");
+      };
+
+      Button crashButton1 = FindViewById<Button>(Resource.Id.crashButton1);
+      crashButton1.Click += delegate {
+        CrashException();
+      };
+
+      Button crashButton2 = FindViewById<Button>(Resource.Id.crashButton2);
+      crashButton2.Click += delegate {
+        CrashStackOverflow();
+      };
+
+      Button crashButton3 = FindViewById<Button>(Resource.Id.crashButton3);
+      crashButton3.Click += delegate {
+        CrashNative();
+      };
     }
 
-    int CrashMe() {
-      return CrashMe();
-    }
-
-    void CrashMe2(string testStr) {
-      Console.WriteLine(testStr.Length);
-    }
-
-    void CrashMe3() {
+    void CrashException() {
       Crashes.GenerateTestCrash();
+    }
+
+    int CrashStackOverflow() {
+      return CrashStackOverflow();
+    }
+
+    void CrashNative() {
+      TestCrash();
     }
   }
 }
